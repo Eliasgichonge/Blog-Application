e84f9ad36ad5d2b79c45823696c339f45afa7555
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-01-28 15:36:07 +0300

    Adding comments to the post detail view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -67,10 +67,16 @@ def post_detail(request, id):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
 
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
 
 
 @require_POST
