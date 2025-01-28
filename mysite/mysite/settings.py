ad439fb7248560cefcb75a7d93199007034c52e4
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-01-28 15:03:05 +0300

    Sending emails with Django

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -130,3 +130,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'eliasmwita86@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
